a047a58c4cd62bc1c9cb416063f4e895504c861c
Zetalasis <zetalasis@gmail.com>
2023-11-14 15:29:43 -0700

    I hate you VS. I had to recreate all my code using dnSpy but now it should be able to be cloned from VS? (I hope) Also new menu features!!

---
+++ ModdersToolKit/Mixins.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+
+namespace ModdersToolKit
+{
+    public class Mixins
+    {
+        public enum MixinType
+        {
+            Inject,
+            Overwrite
+        }
+
+        public enum MixinInjectionPosition
+        {
+            Head,
+            Tail
+        }
+
+        private static HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("com.zetalasis.lethal_company_mtk_patches");
+
+        /**<summary>Stolen from Java, Mixins allow you to "mix into" the games code, allowing you to add custom functions.
+         <para>Be REALLY careful with this function - you WILL break mods if you aren't. (or even break the game)</para>
+        <para>More documentation available on the GitHub wiki.</para></summary>
+         */
+        public static void CreateMixin(MixinType type, MixinInjectionPosition position, object mixInto, object mixFrom)
+        {
+            var mOriginal = AccessTools.Method(mixInto.GetType(), nameof(mixInto));
+
+            if (type == MixinType.Inject)
+            {
+                if (position == MixinInjectionPosition.Head)
+                {
+                    var mPrefix = SymbolExtensions.GetMethodInfo(() => mixFrom.GetType());
+                    harmony.Patch(mOriginal, new HarmonyMethod(mPrefix), null);
+                }
+                if (position == MixinInjectionPosition.Head)
+                {
+                    var mPostFix = SymbolExtensions.GetMethodInfo(() => mixFrom.GetType());
+                    harmony.Patch(mOriginal, null, new HarmonyMethod(mPostFix));
+                }
+            }
+        }
+    }
+}
